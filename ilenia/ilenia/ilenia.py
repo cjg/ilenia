bddbcb5b6ce718e7108b6f18cab0eb5d894e9493
Giuseppe Coviello <cjg@cruxppc.org>
2005-09-06 08:07:01 +0000

    fixed import

--- ilenia/ilenia/ilenia.py
+++ ilenia/ilenia/ilenia.py
@@ -19,8 +19,8 @@
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 """
 
-from packages import Packages
-from confront import *
+from lib.packages import Packages
+from lib.confront import *
 from ConfigParser import ConfigParser
 import os, os.path
 
