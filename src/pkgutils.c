c5de3be084c79014462ae61f70fb88f025652ce4
Giuseppe Coviello <cjg@cruxppc.org>
2005-10-26 12:17:14 +0000

    new update

--- src/pkgutils.c
+++ src/pkgutils.c
@@ -38,31 +38,7 @@
 #include "lspkgs.h"
 #include "repolist.h"
 #include "output.h"
-
-int
-exec (char *wdir, char *command, char *args[])
-{
-  int status;
-  pid_t pid = fork ();
-  if (pid == 0)
-    {
-      if (wdir)
-	chdir (wdir);
-      execv (command, args);
-      exit (EXIT_FAILURE);
-    }
-  else if (pid < 0)
-    {
-      status = EXIT_FAILURE;
-    }
-  else
-    {
-      while ((waitpid (pid, &status, 0) == 0))
-	{
-	}
-    }
-  return (status);
-}
+#include "utils.h"
 
 int
 do_post_pkgadd (char *path)
@@ -88,14 +64,20 @@ do_post_pkgadd (char *path)
 void
 installscript (char *path, char *script)
 {
-  int file = 0;
-  char filename[strlen (path) + strlen (script) + 10];
-  sprintf (filename, "%s/%s", path, script);
-  file = open (filename, O_RDONLY);
-  if (file == 0)
-    return;
-  close (file);
-  exec (path, script, NULL);
+  /*
+     int file = 0;
+     char filename[strlen(path) + strlen(script) + 10];
+     sprintf(filename, "%s/%s", path, script);
+     file = open(filename, O_RDONLY);
+     if (file == 0)
+     return;
+     close(file);
+   */
+  char *filepath = strdup (path);
+  strcat (filepath, "/");
+  strcat (filepath, script);
+  if (is_file (filepath) == EXIT_SUCCESS)
+    exec (path, script, NULL);
 }
 
 int
---
+++ src/utils.c
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *            utils.c
+ *
+ *  Wed Oct 26 12:58:00 2005
+ *  Copyright  2005  Coviello Giuseppe
+ *  immigrant@email.it
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int
+exec (char *wdir, char *command, char *args[])
+{
+  int status;
+  pid_t pid = fork ();
+  if (pid == 0)
+    {
+      if (wdir)
+	chdir (wdir);
+      execv (command, args);
+      exit (EXIT_FAILURE);
+    }
+  else if (pid < 0)
+    {
+      status = EXIT_FAILURE;
+    }
+  else
+    {
+      while ((waitpid (pid, &status, 0) == 0))
+	{
+	}
+    }
+  return (status);
+}
+
+int
+is_file (char *filepath)
+{
+  int file = 0;
+  file = open (filepath, O_RDONLY);
+  if (file == -1)
+    return (EXIT_FAILURE);
+  close (file);
+  return (EXIT_SUCCESS);
+}
